06ae56910de309caf59ceb07ded46114f63f1980
Francois Caen <fracaen@cisco.com>
2021-04-07 20:17:56 -0700

    created terraform-k8s-lab

---
+++ terraform-k8s-lab/deployment.tf
@@ -0,0 +1,45 @@
+resource "kubernetes_deployment" "web-app-deployment" {
+  metadata {
+    name = "my-web-app"
+    labels = {
+      App = "mywebapp"
+    }
+  }
+
+  spec {
+    replicas = 3
+    selector {
+      match_labels = {
+        App = "mywebapp"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          App = "mywebapp"
+        }
+      }
+      spec {
+        container {
+          image = "fracaen/my-web-app"
+          name  = "web-app"
+
+          port {
+            container_port = 5000
+          }
+
+          resources {
+            limits = {
+              cpu    = "0.5"
+              memory = "512Mi"
+            }
+            requests = {
+              cpu    = "250m"
+              memory = "50Mi"
+            }
+          }
+        }
+      }
+    }
+  }
+}
---
+++ terraform-k8s-lab/service.tf
@@ -0,0 +1,20 @@
+resource "kubernetes_service" "web-app-service" {
+  metadata {
+    name = "my-web-app"
+  }
+  spec {
+    selector = {
+      App = kubernetes_deployment.web-app-deployment.spec.0.template.0.metadata[0].labels.App
+    }
+    port {
+      port        = 80
+      target_port = 5000
+    }
+
+    type = "LoadBalancer"
+  }
+}
+
+output "lb_ip" {
+  value = kubernetes_service.web-app-service.status.0.load_balancer.0.ingress.0.hostname
+}
