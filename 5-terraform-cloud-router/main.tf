802a17a5b9b0a08d37d5fd1e5f0bdd892ae29671
Francois Caen <fracaen@cisco.com>
2022-04-01 10:21:21 -0700

    added cloud router to sg_allow_https

--- 5-terraform-cloud-router/main.tf
+++ 5-terraform-cloud-router/main.tf
@@ -22,7 +22,7 @@ resource "aws_instance" "c8kv" {
         availability_zone       = "us-west-2a"
 		subnet_id 				= module.vpc.public_subnets[0]
 
-     	vpc_security_group_ids	= [aws_security_group.sg_allow_ssh.id] 
+     	vpc_security_group_ids	= [aws_security_group.sg_allow_ssh.id,aws_security_group.sg_allow_https.id] 
         key_name                = "FCKeyPair1"
 
 	user_data = <<-EOF
