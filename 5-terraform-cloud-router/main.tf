ea977684ba4161901a0af32f30b2e6242705df6c
Francois Caen <fracaen@cisco.com>
2022-04-01 01:26:59 -0700

    added sg_allow_https for cloud router

--- 5-terraform-cloud-router/main.tf
+++ 5-terraform-cloud-router/main.tf
@@ -41,13 +41,32 @@ EOF
 resource "aws_security_group" "sg_allow_ssh" {
         name                    = "sg_allow_ssh"
         description             = "Allows incoming SSH sessions"
-		vpc_id					= module.vpc.vpc_id
+	vpc_id  		= module.vpc.vpc_id
 
         ingress {
                 from_port       = 22
                 to_port         = 22
                 protocol        = "tcp"
-				cidr_blocks	= ["0.0.0.0/0"]
+		cidr_blocks	= ["0.0.0.0/0"]
+        }
+        egress {
+                from_port       = 0
+                to_port         = 0
+                protocol        = "-1"
+                cidr_blocks     = ["0.0.0.0/0"]
+        }
+}
+
+resource "aws_security_group" "sg_allow_https" {
+        name                    = "sg_allow_https"
+        description             = "Allows incoming HTTPS sessions"
+	vpc_id  		= module.vpc.vpc_id
+
+        ingress {
+                from_port       = 443
+                to_port         = 443
+                protocol        = "tcp"
+		cidr_blocks	= ["0.0.0.0/0"]
         }
         egress {
                 from_port       = 0
